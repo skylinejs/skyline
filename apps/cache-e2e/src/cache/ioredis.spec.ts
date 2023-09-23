e622e648997be1bf04c849a9ed91881e6565e828
skylinejs-dev <skylinejs.dev@gmail.com>
2023-09-23 16:32:25 +0200

    Redis cache storage tests

--- apps/cache-e2e/src/cache/ioredis.spec.ts
+++ apps/cache-e2e/src/cache/ioredis.spec.ts
@@ -1,7 +1,29 @@
-import { RedisCacheStorageEngine, SkylineCache } from '@skylinejs/cache';
+import {
+  CacheConfiguration,
+  RedisCacheStorageEngine,
+  SkylineCache,
+} from '@skylinejs/cache';
 import { execSync } from 'child_process';
-import { join } from 'path';
 import Redis from 'ioredis';
+import { join } from 'path';
+
+const USER_CACHE_NAMESPACE = 'user';
+
+interface UserCache {
+  id: number;
+  name: string;
+}
+
+function isUserCacheOrThrow(value: unknown): asserts value is UserCache {
+  if (typeof value !== 'object') throw new Error();
+  if (typeof (value as any).id !== 'number') throw new Error();
+  if (typeof (value as any).name !== 'string') throw new Error();
+}
+
+const config: Partial<CacheConfiguration> = {
+  defaultCacheExpirationMs: 20_000,
+  randomGeneratorSeed: 'cache-seed-1',
+};
 
 describe('RedisCache: ioredis package', () => {
   const redis = new Redis({
@@ -20,10 +42,214 @@ describe('RedisCache: ioredis package', () => {
     expect(value).toBe('bar');
   });
 
-  it('Connect to redis', async () => {
+  it('Set a (key, value) pair', async () => {
     const cache = new SkylineCache({
+      config,
       storage: new RedisCacheStorageEngine({ redis }),
     });
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-1' },
+      { fetchedAt: Date.now() }
+    );
+    const { value, skipped } = await cache.get(
+      USER_CACHE_NAMESPACE,
+      1,
+      isUserCacheOrThrow
+    );
+
+    expect(value).toEqual({ id: 1, name: 'user-1' });
+    expect(skipped).toBe(false);
+  });
+
+  it('Discard a (key, value) pair if the key already exists', async () => {
+    const cache = new SkylineCache({
+      config,
+      storage: new RedisCacheStorageEngine({ redis }),
+    });
+
+    // Set the value for the first time
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-1' },
+      { fetchedAt: Date.now() }
+    );
+
+    // Expect the value to be set
+    {
+      const { value } = await cache.get(
+        USER_CACHE_NAMESPACE,
+        1,
+        isUserCacheOrThrow
+      );
+      expect(value).toEqual({ id: 1, name: 'user-1' });
+    }
+
+    // Try to set the value again (should be discarded)
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-2' },
+      { fetchedAt: Date.now() }
+    );
+
+    // Expect the value to be unchanged
+    {
+      const { value } = await cache.get(
+        USER_CACHE_NAMESPACE,
+        1,
+        isUserCacheOrThrow
+      );
+      expect(value).toEqual({ id: 1, name: 'user-1' });
+    }
+  });
+
+  it('cache.get: Force a cache skip', async () => {
+    const cache = new SkylineCache({
+      config,
+      storage: new RedisCacheStorageEngine({ redis }),
+    });
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-1' },
+      { fetchedAt: Date.now() }
+    );
+    const { value, skipped } = await cache.get(
+      USER_CACHE_NAMESPACE,
+      1,
+      isUserCacheOrThrow,
+      {
+        skip: 1,
+      }
+    );
+
+    expect(value).toBeUndefined();
+    expect(skipped).toBe(true);
+  });
+
+  it('config.forceCacheSkips: Force a cache skip', async () => {
+    const cache = new SkylineCache({
+      config: { ...config, forceCacheSkips: true },
+      storage: new RedisCacheStorageEngine({ redis }),
+    });
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-1' },
+      { fetchedAt: Date.now() }
+    );
+    const { value, skipped } = await cache.get(
+      USER_CACHE_NAMESPACE,
+      1,
+      isUserCacheOrThrow,
+      {
+        skip: 0,
+      }
+    );
+
+    expect(value).toBeUndefined();
+    expect(skipped).toBe(true);
+  });
+
+  it('cache.disableCacheSkipping: Takes precedence over config.forceCacheSkips', async () => {
+    const cache = new SkylineCache({
+      config: { ...config, forceCacheSkips: true },
+      storage: new RedisCacheStorageEngine({ redis }),
+    });
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-1' },
+      { fetchedAt: Date.now() }
+    );
+    cache.disableCacheSkipping();
+    const { value, skipped } = await cache.get(
+      USER_CACHE_NAMESPACE,
+      1,
+      isUserCacheOrThrow,
+      {
+        skip: 1,
+      }
+    );
+
+    expect(value).toEqual({ id: 1, name: 'user-1' });
+    expect(skipped).toBe(false);
+  });
+
+  it('cache.get: skip=0.5 option results in 50% cache skips', async () => {
+    const cache = new SkylineCache({
+      config,
+      storage: new RedisCacheStorageEngine({ redis }),
+    });
+    await cache.setIfNotExist(
+      USER_CACHE_NAMESPACE,
+      ({ id }) => id,
+      { id: 1, name: 'user-1' },
+      { fetchedAt: Date.now() }
+    );
+
+    // Expect the value to be skipped (test depends on randomGeneratorSeed)
+    {
+      const { value, skipped } = await cache.get(
+        USER_CACHE_NAMESPACE,
+        1,
+        isUserCacheOrThrow,
+        {
+          skip: 0.5,
+        }
+      );
+
+      expect(value).toBeUndefined();
+      expect(skipped).toBe(true);
+    }
+
+    // Expect the value to not be skipped (test depends on randomGeneratorSeed)
+    {
+      const { value, skipped } = await cache.get(
+        USER_CACHE_NAMESPACE,
+        1,
+        isUserCacheOrThrow,
+        {
+          skip: 0.5,
+        }
+      );
+
+      expect(value).toEqual({ id: 1, name: 'user-1' });
+      expect(skipped).toBe(false);
+    }
+
+    // Expect the value to be skipped (test depends on randomGeneratorSeed)
+    {
+      const { value, skipped } = await cache.get(
+        USER_CACHE_NAMESPACE,
+        1,
+        isUserCacheOrThrow,
+        {
+          skip: 0.5,
+        }
+      );
+
+      expect(value).toBeUndefined();
+      expect(skipped).toBe(true);
+    }
+
+    // Expect the value to not be skipped (test depends on randomGeneratorSeed)
+    {
+      const { value, skipped } = await cache.get(
+        USER_CACHE_NAMESPACE,
+        1,
+        isUserCacheOrThrow,
+        {
+          skip: 0.5,
+        }
+      );
+
+      expect(value).toEqual({ id: 1, name: 'user-1' });
+      expect(skipped).toBe(false);
+    }
   });
 
   afterAll(async () => {
