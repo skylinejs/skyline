29745bb53f05e3b5fdc2256ee16d75e71317d012
skylinejs-dev <skylinejs.dev@gmail.com>
2023-10-02 16:01:05 +0200

    env specs

--- packages/env/src/lib/env.spec.ts
+++ packages/env/src/lib/env.spec.ts
@@ -2,10 +2,35 @@ import { SkylineEnv } from './env';
 
 enum RuntimeEnvironment {
   DEV = 'DEV',
+  CI = 'CI',
   PRD = 'PRD',
 }
 
 describe('SkylineEnv', () => {
+  it('Parse string environment variable', () => {
+    const envParser = new SkylineEnv<typeof RuntimeEnvironment>({
+      processEnv: {
+        SERVER_DATABASE_HOST: 'localhost',
+      },
+    });
+
+    const env = {
+      database: {
+        host: envParser.parseString('SERVER_DATABASE_HOST', {
+          DEV: '127.0.0.1',
+          PRD: 'db.example.org',
+        }),
+        port: envParser.parseNumber('SERVER_DATABASE_PORT', {
+          DEV: 5432,
+          PRD: 5433,
+        }),
+      },
+    };
+
+    expect(env.database.host).toBe('localhost');
+    expect(env.database.port).toBe(undefined);
+  });
+
   it('Parse string environment variable', () => {
     const envParser = new SkylineEnv<typeof RuntimeEnvironment>({
       runtime: RuntimeEnvironment.DEV,
@@ -29,5 +54,30 @@ describe('SkylineEnv', () => {
 
     expect(env.database.host).toBe('localhost');
     expect(env.database.port).toBe(5432);
+  });
+
+  it('Parse string environment variable', () => {
+    const envParser = new SkylineEnv<typeof RuntimeEnvironment>({
+      runtime: RuntimeEnvironment.PRD,
+      processEnv: {
+        SERVER_DATABASE_HOST: 'localhost',
+      },
+    });
+
+    const env = {
+      database: {
+        host: envParser.parseString('SERVER_DATABASE_HOST', {
+          DEV: '127.0.0.1',
+          PRD: 'db.example.org',
+        }),
+        port: envParser.parseNumber('SERVER_DATABASE_PORT', {
+          DEV: 5432,
+          PRD: 5433,
+        }),
+      },
+    };
+
+    expect(env.database.host).toBe('localhost');
+    expect(env.database.port).toBe(5433);
   });
 });
