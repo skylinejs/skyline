8c84df804caf56d4774d5c6a0e1c1ba5cbe7f97e
skylinejs-dev <skylinejs.dev@gmail.com>
2023-10-11 17:05:16 +0200

    Install nodemailer for translate email sending example

--- apps/example-translate-nestjs-minimal-e2e/src/example-translate-nestjs-minimal/example-translate-nestjs-minimal.spec.ts
+++ apps/example-translate-nestjs-minimal-e2e/src/example-translate-nestjs-minimal/example-translate-nestjs-minimal.spec.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 describe('SkylineTranslate minimal NestJS example', () => {
   it('Send user registration email', async () => {
-    const res = await axios.get(`/api`);
+    const res = await axios.post(`/api/register`, { email: 'registration1@skylinejs.com' });
     expect(res.data).toEqual({ message: 'Hello API' });
   });
 });
