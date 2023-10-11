55c9a92faf26d5dcb7736827d00668b0bcd0da12
skylinejs-dev <skylinejs.dev@gmail.com>
2023-10-11 16:53:58 +0200

    e2e test translate nestjs example

--- apps/example-translate-nestjs-minimal-e2e/src/example-translate-nestjs-minimal/example-translate-nestjs-minimal.spec.ts
+++ apps/example-translate-nestjs-minimal-e2e/src/example-translate-nestjs-minimal/example-translate-nestjs-minimal.spec.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
+describe('SkylineTranslate minimal NestJS example', () => {
+  it('Send user registration email', async () => {
     const res = await axios.get(`/api`);
-
-    expect(res.status).toBe(200);;
     expect(res.data).toEqual({ message: 'Hello API' });
   });
-})
+});
--- apps/example-translate-nestjs-minimal/src/app/app.controller.ts
+++ apps/example-translate-nestjs-minimal/src/app/app.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
+  @Post('register')
+  sendRegistrationEmail(@Body() input: { email: string }) {
     return this.appService.getData();
   }
 }
