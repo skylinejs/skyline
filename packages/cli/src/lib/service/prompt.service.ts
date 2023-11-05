da103da17f928525c71f0398b33e96ec2858a804
skylinejs-dev <skylinejs.dev@gmail.com>
2023-11-05 10:45:30 +0100

    PromptService

--- packages/cli/src/lib/service/prompt.service.ts
+++ packages/cli/src/lib/service/prompt.service.ts
@@ -1,7 +1,23 @@
-import inquirer, { Answers, PromptModule, QuestionCollection } from 'inquirer';
+import inquirer, {
+  PromptModule,
+  Answers as PromptAnswers,
+  QuestionCollection as PromptQuestionCollection,
+} from 'inquirer';
 
+// Re-export types from inquirer
+export { PromptAnswers, PromptQuestionCollection };
+
+// Re-export inquirer autocomplete prompt
+import AutocompletePrompt from 'inquirer-autocomplete-prompt';
+export { AutocompletePrompt };
+
+/**
+ * Prompt the CLI user for input.
+ * Uses the inquirer package.
+ */
 export class PromptService {
   private inquirerPrompt?: PromptModule;
+  private registeredPromptNames: string[] = [];
 
   private getPromptModule() {
     // Lazy initialization
@@ -13,6 +29,8 @@ export class PromptService {
   }
 
   registerPrompt(name: string, prompt: inquirer.prompts.PromptConstructor) {
+    if (this.registeredPromptNames.includes(name)) return;
+    this.registeredPromptNames.push(name);
     this.getPromptModule().registerPrompt(name, prompt);
   }
 
@@ -20,8 +38,8 @@ export class PromptService {
     this.getPromptModule().restoreDefaultPrompts();
   }
 
-  prompt<T extends Answers = Answers>(
-    questions: QuestionCollection<T>,
+  prompt<T extends PromptAnswers = PromptAnswers>(
+    questions: PromptQuestionCollection<T>,
     initialAnswers?: Partial<T>,
   ): Promise<T> & { ui: inquirer.ui.Prompt<T> } {
     return this.getPromptModule()(questions, initialAnswers);
